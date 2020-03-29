a05d514d6a70a34e27dba72290b0e83b0d381eb9
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-03-28 23:56:35 -0700

    writing encoder struct

--- src/encode.rs
+++ src/encode.rs
@@ -2,32 +2,41 @@ use osmpbf::{Node, Way, DenseNode, TagIter};
 use std::collections::HashMap;
 use crate::types;
 
-pub fn way (way: Way, refs: &HashMap<i64, (f64, f64)>) {
-  let typ = get_type(way.tags());
-  println!("{}", typ)
+pub struct Encoder {
+  all_types: HashMap<String, i32>
 }
 
-pub fn node (node: Node, refs: &HashMap<i64, (f64, f64)>) {
-  let typ = get_type(node.tags());
-  println!("{}", typ)
+impl Encoder {
+  pub fn new () -> Encoder {
+    let all_types= types::get_types();
+    return Encoder { all_types: all_types }
+  }
 
-}
+  pub fn way (&self, way: Way, refs: &HashMap<i64, (f64, f64)>) {
+    let typ = self.get_type(way.tags());
+  }
 
-pub fn dense_node (dense: DenseNode, refs: &HashMap<i64, (f64, f64)>) {
-  // parse_tags(dense.tags())
-}
+  pub fn node (&self, node: Node, refs: &HashMap<i64, (f64, f64)>) {
+    let typ = self.get_type(node.tags());
+  }
 
-fn get_type (tags: TagIter) -> i32 {
-  let all_types= types::get_types();
-  let mut t = None;
-  for tag in tags {
-    let string = format!("{}.{}", tag.0, tag.1);
-    if all_types.contains_key(&string) {
-      t = all_types.get(&string) 
+  pub fn dense_node (&self, dense: DenseNode, refs: &HashMap<i64, (f64, f64)>) {
+    // parse_tags(dense.tags())
+  }
+
+  fn get_type (&self, tags: TagIter) -> i32 {
+    let mut t = None;
+    for tag in tags {
+      let string = format!("{}.{}", tag.0, tag.1);
+      if self.all_types.contains_key(&string) {
+        t = self.all_types.get(&string) 
+      }
+    }
+    match t {
+      Some(_) => return *t.unwrap(),
+      None => return 277 // place.other
     }
   }
-  match t {
-    Some(_) => return *t.unwrap(),
-    None => return 277 // place.other
-  }
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod encode;
 mod types;
 
+use encode::Encoder;
 use std::collections::HashMap;
 use osmpbf::{ElementReader, Element};
 use std::error::Error;
@@ -9,33 +10,36 @@ use std::env;
 fn main() {
     run();
 }
-
+  
 fn run() -> Result<(), Box<dyn Error>>  {
     let args: Vec<String> = env::args().collect();
+
     let reader = ElementReader::from_path(&args[1]).unwrap();
 
     let mut nodes: HashMap<i64, (f64, f64)> = HashMap::new();
     let mut refs: HashMap<i64, (f64, f64)> = HashMap::new();
 
+    let encoder: Encoder = Encoder::new();
+
     reader.for_each(|item| {
         match item {
             Element::DenseNode(dense) => {
                nodes.insert(dense.id, (dense.lat(), dense.lon()));
-               encode::dense_node(dense, &refs);
+               encoder.dense_node(dense, &refs);
             },
             Element::Relation(_rel) => {
                 // do nothing
             },
             Element::Node(node) => {
                nodes.insert( node.id(), (node.lat(), node.lon()));
-               encode::node(node, &refs);
+               encoder.node(node, &refs);
             },
             Element::Way(way) => {
                 for r in way.refs() {
                    let ref item = nodes[&r];
                    refs.entry(r).or_insert(*item);
                 }
-               encode::way(way, &refs);
+               encoder.way(way, &refs);
             }
         }
     }).unwrap();
