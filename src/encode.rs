c5590d5f050a04f9e75f7ca16d9ead5a7242d440
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-03-28 22:18:31 -0700

    get types right

--- src/encode.rs
+++ src/encode.rs
@@ -1,21 +1,6 @@
-use crate::types;
-use std::fs::File;
-use std::io::Read;
-use serde_json;
 use osmpbf::{Node, Way, DenseNode, TagIter};
 use std::collections::HashMap;
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Basic {}
-
-fn all_types () {
-    let path = "./features.json";
-    let mut file = File::open("text.json").unwrap();
-    let mut data = String::new();
-    file.read_to_string(&mut data).unwrap();
-    let json: Basic = serde_json::from_str(&data).unwrap();
-}
+use crate::types;
 
 pub fn way (way: Way, refs: &HashMap<i64, (f64, f64)>) {
   let typ = get_type(way.tags());
@@ -32,12 +17,13 @@ pub fn dense_node (dense: DenseNode, refs: &HashMap<i64, (f64, f64)>) {
   // parse_tags(dense.tags())
 }
 
-fn get_type (tags: TagIter) -> usize {
-  let t: Option<&usize>;
+fn get_type (tags: TagIter) -> i32 {
+  let TYPES = types::get_types();
+  let t: Option<&i32>;
   for tag in tags {
-    t = types::parse(&format!("{}.{}", tag.0, tag.1));
+    let string = format!("{}.{}", tag.0, tag.1);
+    t = TYPES.get(&string);
   }
-
   match t {
     Some(_) => return *t.unwrap(),
     None => return 277 // place.other
