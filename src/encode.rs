9d6b293d540f2f102497d37cd6713ea9f4c31780
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-08-24 16:11:04 -0700

    fix: runs!

--- src/encode.rs
+++ src/encode.rs
@@ -1,4 +1,4 @@
-use osmpbf::{Node, Way, DenseNode};
+use osmpbf::{Node, Way, DenseNode, WayRefIter};
 use failure::Error;
 use std::collections::HashMap;
 use desert::ToBytesLE;
@@ -6,24 +6,13 @@ use crate::schema::{PeerLine, PeerArea, PeerNode, Tag};
 
 const PLACE_OTHER: i32 = 277;
 
-fn is_area (refs: &[i64]) -> bool {
-  let len = refs.len();
-  if len < 3 {
-    return false;
-  } else {
-    let first = refs[0];
-    let last = refs[len - 1];
-    return first == last;
-  }
-}
-
-fn get_positions (refs: &[i64], deps: &HashMap<i64, (f64, f64)>) -> Vec<(f32, f32)> {
+fn get_positions (refs: WayRefIter, deps: &HashMap<i64, (f64, f64)>) -> Vec<(f32, f32)> {
   let mut positions = Vec::new();
   // positions
   for r in refs {
     let lat;
     let lon;
-    match deps.get(r) {
+    match deps.get(&r) {
       Some(dep) => {
         lon = dep.0;
         lat = dep.1;
@@ -41,14 +30,20 @@ pub fn way (way: Way, deps: &HashMap<i64, (f64, f64)>) -> Result<Vec<u8>, Error>
     .map(|a| Tag { K: String::from(a.0), V: String::from(a.1) })
     .collect();
 
-  let refs = way.raw_refs();
+  let mut refs = way.refs();
   let len = refs.len();
-  if is_area(refs) {
-    let area = PeerArea { id: way.id(), positions: get_positions(refs, deps), tags };
-    let buf = area.to_bytes_le()?;
-    return Ok(buf);
-  } else if len > 1 {
-    let line = PeerLine { id: way.id(), positions: get_positions(refs, deps), tags };
+  if len >= 3 {
+    let first = refs.next();
+    let last = refs.last();
+    if first == last {
+      let area = PeerArea { id: way.id(), positions: get_positions(way.refs(), deps), tags };
+      let buf = area.to_bytes_le()?;
+      return Ok(buf);
+    }
+  }
+
+  if len > 1 {
+    let line = PeerLine { id: way.id(), positions: get_positions(way.refs(), deps), tags };
     let buf = line.to_bytes_le()?;
     return Ok(buf);
   } else {
