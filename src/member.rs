ac0f8a0f87fb0012d6ba3da727278ed8779e44c0
substack <substack@bits.coop>
2021-03-01 14:57:17 -1000

    using loop over connected elements for sorting

--- src/member.rs
+++ src/member.rs
@@ -31,20 +31,28 @@ impl Member {
         members.drain_filter(|m| {
             let ref_len = ways.get(&(m.id as i64)).and_then(|refs| Some(refs.len()));
             match (&m.role,&m.member_type,ref_len) {
-                (MemberRole::Inner(),MemberType::Way(),Some(len)) => len > 0,
-                (MemberRole::Outer(),MemberType::Way(),Some(len)) => len > 0,
-                _ => false,
+                (MemberRole::Inner(),MemberType::Way(),Some(len)) => len == 0,
+                (MemberRole::Outer(),MemberType::Way(),Some(len)) => len == 0,
+                _ => true,
             }
         });
     }
     pub fn sort(members: &[Member], ways: &HashMap<i64, Vec<i64>>) -> Vec<Member> {
         if members.is_empty() { return vec![] }
-        let mut first_ids: HashMap<i64,usize> = HashMap::new();
-        let mut last_ids: HashMap<i64,usize> = HashMap::new();
+        let mut first_ids: HashMap<i64,Vec<usize>> = HashMap::new();
+        let mut last_ids: HashMap<i64,Vec<usize>> = HashMap::new();
         for (i,m) in members.iter().enumerate() {
             let refs = ways.get(&(m.id as i64)).unwrap();
-            first_ids.insert(*refs.first().unwrap(), i);
-            last_ids.insert(*refs.last().unwrap(), i);
+            let fi = refs.first().unwrap();
+            let li = refs.last().unwrap();
+            match first_ids.get_mut(fi) {
+                None => { first_ids.insert(*fi, vec![i]); },
+                Some(ii) => ii.push(i),
+            }
+            match last_ids.get_mut(li) {
+                None => { last_ids.insert(*li, vec![i]); },
+                Some(ii) => ii.push(i),
+            }
         }
         let mut i = 0;
         let mut j = 0;
@@ -70,27 +78,50 @@ impl Member {
             let refs = ways.get(&id).unwrap();
             let first = refs.first().unwrap();
             let last = refs.last().unwrap();
-            let fif = first_ids.get(first).unwrap();
-            let lif = last_ids.get(first).unwrap();
-            let fil = first_ids.get(last).unwrap();
-            let lil = last_ids.get(last).unwrap();
-            if !visited.contains(&fif) {
-                i = *fif;
-                sorted.last_mut().unwrap().reverse = true;
-                reverse = false;
-            } else if !visited.contains(&lif) {
-                i = *lif;
-                sorted.last_mut().unwrap().reverse = true;
-                reverse = true;
-            } else if !visited.contains(&fil) {
-                i = *fil;
-                sorted.last_mut().unwrap().reverse = false;
-                reverse = false;
-            } else if !visited.contains(&lil) {
-                i = *lil;
-                sorted.last_mut().unwrap().reverse = false;
-                reverse = true;
-            } else {
+
+            let efifs = vec![];
+            let elifs = vec![];
+            let efils = vec![];
+            let elils = vec![];
+            let fifs = first_ids.get(first).or(Some(&efifs)).unwrap();
+            let lifs = last_ids.get(first).or(Some(&elifs)).unwrap();
+            let fils = first_ids.get(last).or(Some(&efils)).unwrap();
+            let lils = last_ids.get(last).or(Some(&elils)).unwrap();
+
+            let max_k = fifs.len().max(lifs.len()).max(fils.len()).max(lils.len());
+            let mut found = false;
+            for k in 0..max_k {
+                let fif = fifs.get(k);
+                let lif = lifs.get(k);
+                let fil = fils.get(k);
+                let lil = lils.get(k);
+                if fif.is_some() && !visited.contains(&fif.unwrap()) {
+                    i = *fif.unwrap();
+                    sorted.last_mut().unwrap().reverse = false;
+                    reverse = false;
+                    found = true;
+                    break;
+                } else if lif.is_some() && !visited.contains(&lif.unwrap()) {
+                    i = *lif.unwrap();
+                    sorted.last_mut().unwrap().reverse = true;
+                    reverse = true;
+                    found = true;
+                    break;
+                } else if fil.is_some() && !visited.contains(&fil.unwrap()) {
+                    i = *fil.unwrap();
+                    sorted.last_mut().unwrap().reverse = false;
+                    reverse = true;
+                    found = true;
+                    break;
+                } else if lil.is_some() && !visited.contains(&lil.unwrap()) {
+                    i = *lil.unwrap();
+                    //sorted.last_mut().unwrap().reverse = false;
+                    reverse = false;
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
                 i = j;
                 j += 1;
             }
