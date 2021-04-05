3893fd43413befffdeecde415ccfbe9750c723b8
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-04-04 20:31:16 -0700

    Add tag priorities file

---
+++ src/tag_priorities.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+pub fn get_priorities<'a>() -> HashMap<&'a str, u64> {
+    return [
+        ("aerialway.cable_car": 100),
+        ("aerialway.chair_lift": 100),
+        ("aeroway.aerodrome", 100),
+        ("amenity.*", 60),
+        ("amenity.university", 100),
+        ("boundary.*", 5),
+        ("building.*", 20),
+        ("building.yes", 20),
+        ("craft.*", 10),
+        ("landuse.*", 10),
+        ("man_made.lighthouse", 70),
+        ("military.*", 70),
+        ("place.*", 10),
+        ("power.*", 100),
+        ("public_transport.*", 50),
+        ("railway.*", 100),
+        ("routes.*", 10),
+        ("sport.*", 1)
+    ]
+}
